4c831120a794a32803e802089fcd455b47224788
pintosoft <master@pintosoft.com>
2022-08-24 13:18:03 +0900

    쿠팡 다이나믹 배너 iframe 태그 방식으로 변경

--- js/adva.js
+++ js/adva.js
@@ -9,7 +9,7 @@ var adva = {
 		html + '</div>');
 	},
 	call518: function () {
-		adva.call(518, 200, '<script src="https://ads-partners.coupang.com/g.js"></script><script>new PartnersCoupang.G({"id":556083,"template":"carousel","trackingCode":"AF4463963","width":"518","height":"200"});</script>');
+		adva.call(518, 200, '<iframe src="https://ads-partners.coupang.com/widgets.html?id=556083&template=carousel&trackingCode=AF4463963&subId=&width=518&height=200" width="518" height="200" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>');
 	},
 	call728: function () {
 		adva.call(728, 90, '<iframe scrolling="no" src="https://minishop.linkprice.com/productbox_realhotdeal.php?affiliate_id=A100513530&category=MD%E2%80%99S%20PICK%7C%ED%8C%A8%EC%85%98%C2%B7%EB%B7%B0%ED%8B%B0%7C%EA%B0%80%EC%A0%84%C2%B7%EB%94%94%EC%A7%80%ED%84%B8%7C%EC%8B%9D%ED%92%88%C2%B7%EC%83%9D%ED%99%9C%C2%B7%EC%9C%A0%EC%95%84%EB%8F%99%7C%EC%97%AC%ED%96%89%C2%B7%ED%8B%B0%EC%BC%93&width=728&height=90" width="728" height="90" border="0" marginwidth="0" marginheight="0" leftmargin="0" topmargin="0" frameborder="0"></iframe>');
